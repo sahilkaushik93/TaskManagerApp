de9ae3411bb1db753f3a06c6d43d775b744aebe1
Sahil Kaushik <kaushiksahil1412@gmail.com>
2023-11-23 21:54:39 +0530

    adding final code dated - 23/11/2023

--- app.py
+++ app.py
@@ -1,61 +1,58 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    start_date = db.Column(db.DateTime, default=datetime.utcnow)
+    completion_date = db.Column(db.DateTime, nullable=True)
+    priority = db.Column(db.String(20), default="Normal")
+
+with app.app_context():
+    # Create the database tables
+    db.create_all()
 
 @app.route('/')
 def index():
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('index.html', tasks=tasks, completed_tasks=completed_tasks, today=datetime.now().strftime("%Y-%m-%d"))
+    tasks = Task.query.order_by(Task.completed, Task.start_date).all()
+    return render_template('index.html', tasks=tasks)
 
-@app.route('/add_task', methods=['POST'])
+@app.route('/add', methods=['POST'])
 def add_task():
-    if request.method == 'POST':
-        task_content = request.form['task_content']
-        new_task = Task(content=task_content)
+    task_content = request.form['task_content']
+    priority = request.form.get('priority', 'Normal')  # Default to 'Normal' if not provided
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task.'
+    new_task = Task(content=task_content, priority=priority, start_date=datetime.utcnow())
+    db.session.add(new_task)
+    db.session.commit()
 
-@app.route('/complete_task/<int:task_id>', methods=['GET', 'POST'])
-def complete_task(task_id):
-    task_to_complete = Task.query.get_or_404(task_id)
+    return redirect(url_for('index'))
 
-    if request.method == 'POST':
-        task_to_complete.completed = True
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue completing the task.'
-
-    return render_template('index.html')  # Add the template name here
-
-@app.route('/delete_task/<int:task_id>', methods=['POST'])
+@app.route('/delete/<int:task_id>', methods=['POST'])
 def delete_task(task_id):
     task_to_delete = Task.query.get_or_404(task_id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect(url_for('index'))
 
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was an issue deleting the task.'
+@app.route('/complete/<int:task_id>', methods=['POST'])
+def complete_task(task_id):
+    task_to_complete = Task.query.get_or_404(task_id)
+    task_to_complete.completed = True
+    task_to_complete.completion_date = datetime.utcnow()
+    db.session.commit()
+    return redirect(url_for('index'))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
